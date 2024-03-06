507f707288963367560698e9510ae084cabdf43a
aladindaoud <83234713+aladindaoud@users.noreply.github.com>
2024-03-06 07:22:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-const express = require("express");
-const cors = require("cors");
-
 const app = express();
+const cors = require("cors");
 
 var corsOptions = {
   origin: "*"
 };
 
 app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
@@ -29,15 +23,11 @@ db.mongoose
     process.exit();
   });
 
-// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
 require("./app/routes/turorial.routes")(app);
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+module.exports = app; // exporting app for vercel
+
