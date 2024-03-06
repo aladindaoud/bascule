dfb5a3417544a5f06c0020829ce8676fe55e2bf8
aladindaoud <83234713+aladindaoud@users.noreply.github.com>
2024-03-06 07:48:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const app = express();
 const cors = require("cors");
 
-var corsOptions = {
-  origin: "http://localhost:4200"
+
+const allowedOrigins= ['http://localhost:4200/', '*'];
+var corsOptionsDelegate = (req, callback) => {
+var corsOptions;
+  
+console.log(req.header('Origin'));
+if(allowedOrigins.indexOf(req.header('Origin')) !== -1) {
+corsOptions = { origin: true };
+}
+else {
+corsOptions = { origin: false };
+}
+callback(null, corsOptions);
 };
+
+
+
+
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -28,6 +43,9 @@ app.get("/", (req, res) => {
 });
 
 require("./app/routes/turorial.routes")(app);
+
+exports.cors = cors();
+exports.corsWithOptions = cors(corsOptionsDelegate);
 
 module.exports = app; // exporting app for vercel
 
